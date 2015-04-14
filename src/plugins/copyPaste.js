0c6d09527da3ea09d1e9e835c9736a8df13e6cf5
Nedim Cholich <nedim@frontyard.co>
2015-04-14 16:11:26 +0200

    Rearranged the hook arguments so that inputArray becomes first.

--- src/plugins/copyPaste.js
+++ src/plugins/copyPaste.js
@@ -43,7 +43,7 @@
       });
 
       // If hook returns falsy value don't proceed with paste.
-      if(Handsontable.hooks.execute(instance, 'beforePaste', areaStart.row, areaStart.col, areaEnd.row, areaEnd.col, inputArray, instance.getSettings().pasteMode)) {
+      if(instance.runHooks(instance, 'beforePaste', inputArray, areaStart.row, areaStart.col, areaEnd.row, areaEnd.col, instance.getSettings().pasteMode)) {
         instance.populateFromArray(areaStart.row, areaStart.col, inputArray, areaEnd.row, areaEnd.col, 'paste', instance.getSettings().pasteMode);
       }
     };
